ece6d3ea58cdd7c20a2fa06e1e6b41d162a1d76e
chris.zen <fzhenmei@gmail.com>
2012-03-21 13:58:09 +0800

    add Windows Console Host demo

---
+++ WcfClient/Demos/Demo1.2.2/Client/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Client.ServiceReference1;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var client = new Service1Client();
+
+            while (true)
+            {
+                var input = Console.ReadLine();
+                var i = 0;
+                int.TryParse(input, out i);
+                var result = client.GetData(i);
+                Console.WriteLine(result);
+
+            }
+        }
+    }
+}
---
+++ WcfClient/Demos/Demo1.2.2/Server/Service1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Server
+{
+    // 注意: 如果更改此处的类名 "Service1"，也必须更新 App.config 中对 "Service1" 的引用。
+    public class Service1 : IService1
+    {
+        #region IService1 成员
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        #endregion
+    }
+}
