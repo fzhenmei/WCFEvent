4d372e245176d396f772415f81379bef667fff4c
chris.zen <fzhenmei@gmail.com>
2012-03-21 15:42:25 +0800

    add a demo

---
+++ WcfClient/Demos/Demo3.1/Contract/IService1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Contract
+{
+    // 注意: 如果更改此处的接口名称 "IService1"，也必须更新 App.config 中对 "IService1" 的引用。
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetData(int value);
+    }
+}
