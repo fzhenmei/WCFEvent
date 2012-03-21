3342cb0d2215a99ce3ff971c4fecd8edd9646dd4
chris.zen <fzhenmei@gmail.com>
2012-03-21 16:19:39 +0800

    add demo

---
+++ WcfClient/Demos/Demo3.3/Client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using Contract;
+
+namespace Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Client";
+
+            for (int i = 0; i < 100; i++)
+            {
+                var factory = new ChannelFactory<IService1>("MyServer");
+                var client = factory.CreateChannel();
+                var result = client.GetData(i);
+                Console.WriteLine(result);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ WcfClient/Demos/Demo3.3/Server/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+
+namespace Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Server";
+
+            var host = new ServiceHost(typeof(Service1));
+            host.Open();
+
+            Console.ReadKey();
+        }
+    }
+}
