77a6a34fad871605d52930c98dec881f17c435b8
chris.zen <fzhenmei@gmail.com>
2012-03-21 17:32:36 +0800

    add demo

---
+++ WcfClient/Demos/Demo3.6.1/Client/Global.asax.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.SessionState;
+using Autofac.Integration.Web;
+using Autofac;
+
+namespace Client
+{
+    public class Global : System.Web.HttpApplication
+    {
+        static IContainerProvider containerProvider;
+
+        public IContainerProvider ContainerProvider
+        {
+            get
+            {
+                return containerProvider;
+            }
+        }
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<IServices>();
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
